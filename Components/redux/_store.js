9706b77d5545a41f69c14913122c816eeebed34f
HSJPRIME <tjdwns5123@gmail.com>
2020-03-16 20:39:52 +0900

    moded

--- Components/redux/_store.js
+++ Components/redux/_store.js
@@ -1,20 +1,19 @@
-import { createStore , applyMiddleware , combineReducers } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from "redux";
 
-import loginReducer from './LoginReducer.js'
-import registerReducer from './RegisterReducer.js'
-import writeReducer from './WriteReducer.js'
-import articleLoaderReducer from './ArticleLoaderReducer.js'
+import loginReducer from "./LoginReducer.js";
+import registerReducer from "./RegisterReducer.js";
+import writeReducer from "./WriteReducer.js";
+import articleLoaderReducer from "./ArticleLoaderReducer.js";
 
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
-
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 let originReducer = combineReducers({
-    login : loginReducer,
-    register : registerReducer,
-    write : writeReducer,
-    articleLoader : articleLoaderReducer
-})
-let _store = createStore(originReducer , applyMiddleware(logger , thunk))
+  login: loginReducer,
+  register: registerReducer,
+  write: writeReducer,
+  articleLoader: articleLoaderReducer
+});
+let _store = createStore(originReducer, applyMiddleware(logger, thunk));
 
-export default _store
+export default _store;
